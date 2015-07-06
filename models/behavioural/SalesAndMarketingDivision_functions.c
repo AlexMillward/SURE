9dcf02975e8a87ace82edee896f0e137644b6546
U-ALEX-XPS\Alex <millwarda@googlemail.com>
2015-07-06 09:20:12 +0100

    Minor addition to SMD

--- models/behavioural/SalesAndMarketingDivision_functions.c
+++ models/behavioural/SalesAndMarketingDivision_functions.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "header.h"
 #include "SalesAndMarketingDivision_agent_header.h"
 
@@ -32,9 +33,65 @@ int SMD_strategise() {
 
   START_DIVISION_MARKETING_INVENTORY_REPORT_MESSAGE_LOOP
 
-    
+    // Find change in sales, prepare previous sales for next iteration
+    CHANGE_IN_SALES = division_marketing_inventory_report_message->dispatched -
+      PREVIOUS_SALES;
+    PROPORTION_SALES_CHANGE = ((double) CHANGE_IN_SALES) / ((double) PREVIOUS_SALES);
+    PREVIOUS_SALES = division_marketing_inventory_report_message->dispatched;
 
   FINISH_DIVISION_MARKETING_INVENTORY_REPORT_MESSAGE_LOOP
+
+  // Initialisation
+  int s, i, history_index;
+  double scores[MARKETING_FACTOR_COUNT];
+  double cost_sum = 0;
+
+  // Find sum of costs
+  for (s=0; s<MARKETING_FACTOR_COUNT; s++) {
+    cost_sum += FACTORS[s].cost;
+  }
+
+  // Calculate scores
+  for (s=0; s<MARKETING_FACTOR_COUNT; s++) {
+    scores[s] = FACTORS[s].cost / cost_sum;
+    for (i=1; i<(HISTORY.size); i++) {
+      history_index = HISTORY.size - i;
+      scores[s] += ( 1.0 / pow(FACTOR_ALPHA, i) ) *
+        (HISTORY.array[history_index].proportion_sales_change /
+        (FACTOR_BETA * HISTORY.array[history_index - 1].factor_proportions[s]) );
+    }
+  }
+
+  // Normalise scores
+  double score_sum = 0;
+  for (s=0; s<MARKETING_FACTOR_COUNT; s++) {
+    score_sum += scores[s];
+  }
+  for (s=0; s<MARKETING_FACTOR_COUNT; s++) {
+    scores[s] /= score_sum;
+  }
+
+  // Purchase factors
+  for (s=0; s<MARKETING_FACTOR_COUNT; s++) {
+    FACTORS[s].available += (scores[s] * FUNDS) / FACTORS[s].cost;
+    FUNDS -= FACTORS[s].available * FACTORS[s].cost;
+  }
+
+  // Calculate factor total
+  double factor_total = 0;
+  for (s=0; s<MARKETING_FACTOR_COUNT; s++) {
+    factor_total += FACTORS[s].available;
+  }
+
+  // Update history
+  double history_additions[MARKETING_FACTOR_COUNT];
+  for (s=0; s<MARKETING_FACTOR_COUNT; s++) {
+    history_additions[s] = FACTORS[s].available;
+  }
+  add_division_sales_history(&HISTORY, PROPORTION_SALES_CHANGE, (&history_additions)[MARKETING_FACTOR_COUNT]);
+  if (HISTORY.size > HISTORY_SIZE_LIMIT) {
+    remove_division_sales_history(&HISTORY, 0);
+  }
 
   return 0;
 
